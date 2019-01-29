3130076838e4cdef6f26aa3232aa15abb1c1ce40
rosario89 <lafacerosario@gmail.com>
2019-01-29 22:48:11 +0100

    adding new features

--- startbootstrap-shop-item-gh-pages/pannello_controllo/index.php
+++ startbootstrap-shop-item-gh-pages/pannello_controllo/index.php
@@ -103,7 +103,7 @@
                     <a href="media_server.html" class="list-group-item">Media Server</a>
 					<a href="http://<?php echo $_SERVER['HTTP_HOST']; ?>:8200" class="list-group-item">Media Server Status</a>
 					<a href="#" class="list-group-item">Database</a>
-					<a href="#" class="list-group-item">File Browser</a>
+					<a href="http://<?php echo $_SERVER['HTTP_HOST']; ?>:37961" class="list-group-item">File Browser</a>
 					<a href="http://<?php echo $_SERVER['HTTP_HOST']; ?>:8081" class="list-group-item">Camera</a>
                 </div>
             </div>
