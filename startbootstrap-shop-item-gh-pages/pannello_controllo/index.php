4e9d564c31f7d7bc4913daa0cc0baa6d0f8da664
rosario89 <lafacerosario@gmail.com>
2019-04-02 23:43:04 +0200

    adding home assistant button

--- startbootstrap-shop-item-gh-pages/pannello_controllo/index.php
+++ startbootstrap-shop-item-gh-pages/pannello_controllo/index.php
@@ -102,6 +102,7 @@
                     <a href="carica_file.html" class="list-group-item">File Upload</a>
                     <a href="media_server.html" class="list-group-item">Media Server</a>
 					<a href="http://<?php echo $_SERVER['HTTP_HOST']; ?>:8200" class="list-group-item">Media Server Status</a>
+                    <a href="http://<?php echo $_SERVER['HTTP_HOST']; ?>:8123" class="list-group-item">Home Assistant</a>
 					<a href="#" class="list-group-item">Database</a>
 					<a href="http://<?php echo $_SERVER['HTTP_HOST']; ?>:37961" class="list-group-item">File Browser</a>
 					<a href="http://<?php echo $_SERVER['HTTP_HOST']; ?>:8081" class="list-group-item">Camera</a>
