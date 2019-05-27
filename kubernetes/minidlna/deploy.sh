d37e6843c65045cac6e8afc8a0cfab2bdb305429
Rosario Laface <rosario.laface@nttdata.com>
2019-05-27 12:31:11 +0200

    adding envtpl, template files and readme

---
+++ kubernetes/minidlna/deploy.sh
@@ -0,0 +1,7 @@
+export EXT_HD_PATH="/mnt/pi/extHD"
+
+#Build step, template files will be filled with env parameters
+envtpl < resources/persistantVolume.yaml.tpl > "persistantVolume.yaml"
+
+#Deploy step, applying k8s resources
+kubectl apply -f persistantVolume.yaml
