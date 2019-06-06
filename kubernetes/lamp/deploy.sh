7ed14d7deaaf45c81c2f89f39b77c2aff5c7a60e
Rosario Laface <rosario.laface@nttdata.com>
2019-06-06 10:58:35 +0200

    adding circleCI configuration and deploy scripts

--- kubernetes/lamp/deploy.sh
+++ kubernetes/lamp/deploy.sh
@@ -1,6 +1,7 @@
 #Cronjobs for external jobs
+#TODO: launch add_cronjob.sh as a k8s cronjob
 
-
+info "Installing server control panel Kubernetes resources"
 #Kubernetes jobs
 kubectl apply -f resources/persistantVolume.yaml
 kubectl apply -f resources/persistantVolumeClaim.yaml
