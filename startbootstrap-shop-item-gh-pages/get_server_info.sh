a803d4059c272ba4998efb5acbf6c35fc3b32862
Rosario Laface <rosario.laface@nttdata.com>
2019-06-12 17:07:20 +0200

    moving scripts to other branch

--- startbootstrap-shop-item-gh-pages/get_server_info.sh
+++
@@ -1,5 +0,0 @@
-MEMORIA_USATA=$(df -h / | awk '{ print $3 }' | tail -n 1 | sed 's/[^0-9|\.]*//g') 
-MEMORIA_TOTALE=$(df -h / | awk '{ print $2 }' | tail -n 1 | sed 's/[^0-9|\.]*//g' )
-TEMPERATURA=$(/opt/vc/bin/vcgencmd measure_temp | sed 's/[^0-9|\.]*//g')
-
-echo '<root><memoriaUsata>'$MEMORIA_USATA'</memoriaUsata><memoriaTotale>'$MEMORIA_TOTALE'</memoriaTotale><temperatura>'$TEMPERATURA'</temperatura></root>' > /tmp/my_xml.xml
