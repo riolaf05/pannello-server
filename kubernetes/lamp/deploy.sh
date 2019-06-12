6295a35c44896058643247d87f97f22094312648
Rosario Laface <rosario.laface@nttdata.com>
2019-06-12 15:32:24 +0200

    k8s server status - adding script to get server node status xml

--- kubernetes/lamp/deploy.sh
+++ kubernetes/lamp/deploy.sh
@@ -1,9 +1,20 @@
 #!/bin/bash
 
+N_CLUSTER=2 #Select cluster node number
+
+echo "Writing XML configuration file for each node of the cluster"
+HOSTNAME=$(hostname)
+printf "<root>\n" >> test.txt
+for (( c=1; c<=$N_CLUSTER; c++)); do printf "\t<"$HOSTNAME">\n\t\t<temperatura>TEMP_"$HOSTNAME"</temperatura>\n\t\t<memoria_act>MEM__ACT_"$HOSTNAME"</memoria_act>\n\t\t<memoria_tot>MEM__TOT_"$(hostname)"</memoria_tot>\n\t</"$HOSTNAME">\n" >> test.txt; done
+printf "</root>\n" >> test.txt
+
+#todo: avvia rsync
+
+#todo: avvia cronjobs per la scrittura di node_param
 #Cronjobs for external jobs
 echo "Installing cronjobs"
-chmod +x scripts/*
-sed 's|{{ .EXT_HD_PATH }}|/scripts/nodes_params.xml|g' script/ > /tmp/persistantVolume.yaml
+#chmod +x scripts/*
+#sed 's|{{ .EXT_HD_PATH }}|$temperatura|g' script/ > /tmp/persistantVolume.yaml
 
 #TODO: launch add_cronjob.sh as a k8s cronjob
 
