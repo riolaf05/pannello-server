59d19bdcf17ee2e7b3837436009941f558643ecd
Rosario Laface <rosario.laface@nttdata.com>
2019-06-12 18:48:03 +0200

    server status - updating scripts to deploy

--- kubernetes/lamp/scripts/add_cronjobs.sh
+++ kubernetes/lamp/scripts/add_cronjobs.sh
@@ -2,7 +2,7 @@ echo "Updating cronjobs"
 echo "Writing out current crontabs"
 crontab -l > mycron
 echo "Writing out new crontabs"
-echo "* * * * * scripts/get_server_info.sh" >> mycron
+echo "* * * * * $HOME/Scripts/get_server_info.sh" >> mycron
 #echo "* * * * * /opt/vc/bin/vcgencmd measure_temp > /tmp/temperatura.txt" >> mycron
 #echo "* * * * * df -h / > /tmp/memoria.txt" >> mycron
 echo "Installing new cron file"
