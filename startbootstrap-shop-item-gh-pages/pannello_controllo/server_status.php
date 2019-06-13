10d8cdfbf90eaad9b3cc0a2b29bea5399cadff91
Rosario Laface <rosario.laface@nttdata.com>
2019-06-13 10:37:52 +0200

    new UI - revert to server_status.php

--- startbootstrap-shop-item-gh-pages/pannello_controllo/server_status.php
+++ startbootstrap-shop-item-gh-pages/pannello_controllo/server_status.php
@@ -28,46 +28,35 @@
 
 <body>
 
-    <?php 
-        //Scrittura temperatura CPU (grazie all'applicazione acpi) e memoria restante
-        #$comando=shell_exec('/opt/vc/bin/vcgencmd measure_temp > /tmp/temperatura.txt && df -h / > /tmp/memoria.txt');
-        
-        //Lettura temperatura CPU 
-        /*
-        $fp = fopen('/tmp/temperatura.txt', r);
-        if(!$fp) {
-            $temperatura=0;
-            }
-        fseek($fp, 5, SEEK_SET); //Mi posiziono al 5° carattere
-        $temperatura = fread($fp, 4); //Leggo 4 caratteri partendo dalla posizione corrente
-        fclose($fp);
-        
+	<?php 
+		//Scrittura temperatura CPU (grazie all'applicazione acpi) e memoria restante
+		#$comando=shell_exec('/opt/vc/bin/vcgencmd measure_temp > /tmp/temperatura.txt && df -h / > /tmp/memoria.txt');
+		
+		//Lettura temperatura CPU 
+		$fp = fopen('/tmp/temperatura.txt', r);
+		if(!$fp) {
+			$temperatura=0;
+			}
+		fseek($fp, 5, SEEK_SET); //Mi posiziono al 5° carattere
+		$temperatura = fread($fp, 4); //Leggo 4 caratteri partendo dalla posizione corrente
+		fclose($fp);
+		
 
-        //lettura spazio disponibile hard disk 
-        $fp = fopen('/tmp/memoria.txt', r);
-        if(!$fp) {
-            $memoria_percentuale=0;
-            }
-        fseek($fp, 66, SEEK_SET);
-        $memoria_tot = fread($fp, 2);
-        fseek($fp, 71, SEEK_SET); 
-        $memoria_usata = fread($fp, 2);
-        fseek($fp, 82, SEEK_SET); 
-        $memoria_percentuale = fread($fp, 2);
-        fclose($fp);
-        */
+		//lettura spazio disponibile hard disk 
+		$fp = fopen('/tmp/memoria.txt', r);
+		if(!$fp) {
+			$memoria_percentuale=0;
+			}
+		fseek($fp, 66, SEEK_SET);
+		$memoria_tot = fread($fp, 2);
+		fseek($fp, 71, SEEK_SET); 
+		$memoria_usata = fread($fp, 2);
+		fseek($fp, 82, SEEK_SET); 
+		$memoria_percentuale = fread($fp, 2);
+		fclose($fp);
+		
 
-        //Lettura temperatura CPU 
-        $xml=simplexml_load_file("/tmp/my_xml.xml") or die("Error: Cannot create XML object");
-        $memoria_usata_1=$xml->$memoriaUsata
-        $memoria_tot_1=$xml->$memoriaTotale
-        $temperatura_1=$xml->$temperatura
-        $memoria_percentuale=40
-
-
-
-
-    ?>
+	?>
 
     <!-- Navigation -->
     <nav class="navbar navbar-inverse navbar-fixed-top" role="navigation">
@@ -113,7 +102,7 @@
                     <a href="carica_file.php" class="list-group-item">Board</a>
                     <a href="#" class="list-group-item">Server Status</a>
                     <a href="#" class="list-group-item">Internet of Things</a>
-                    <a href="http://<?php echo $_SERVER['HTTP_HOST']; ?>:8081" class="list-group-item">Camera Monitor</a>
+					<a href="http://<?php echo $_SERVER['HTTP_HOST']; ?>:8081" class="list-group-item">Camera Monitor</a>
                     <a href="http://<?php echo $_SERVER['HTTP_HOST']; ?>:8123" class="list-group-item">Home Assistant</a>
                     <a href="carica_file.php" class="list-group-item">File Browser</a>
                     <a href="http://<?php echo $_SERVER['HTTP_HOST']; ?>:8200" class="list-group-item">Media Server</a>
@@ -122,34 +111,24 @@
 
             <div class="col-md-8 col-md-offset-1">
 
-
-
-
-
-
                 <h2 style="text-transform: capitalize; color: blue; text-align: center; font-family: Georgia, Serif; ">Dashboard</h2>
-                <h4>Temperatura CPU</h4>
-                <div class="progress progress-striped">
-                  <?php echo $temperatura."° C"; ?><div class="progress-bar progress-bar-danger" style="width: <?php echo $temperatura;?>%;"></div>
-                </div>
-                <h4>Memoria Disponibile</h4>
-                <div class="progress progress-striped">
-                  <?php echo $memoria_usata_1."/".$memoria_tot_1." GB"; ?><div class="progress-bar progress-bar-info" style="width: <?php echo $memoria_percentuale;?>%;"></div>
-                </div>
-                
-                <form method="POST" action='spegni_riavvia.php'>
-                <input type="submit" name="shutdown" value="Server Shutdown" class="btn btn-primary btn-large btn-block">
-                </form>
-                <div style="height:10px";"></div> <!-- div usato per distanziare verticalmente!! -->
-                <form method="POST" action='spegni_riavvia.php'>
-                <input type="submit" name="reboot" value="Server Reboot" class="btn btn-primary btn-large btn-block">
-                </form>
-                
-                
-                <div class="col-md-4 col-md-offset-1">Current CircleCI pipeline status: <a href="https://circleci.com/gh/riolaf05/pannello-server/tree/master"><img src="https://circleci.com/gh/riolaf05/pannello-server/tree/master.svg?style=svg"></a></div>
-
-
-
+				<h4>Temperatura CPU</h4>
+				<div class="progress progress-striped">
+				  <?php echo $temperatura."° C"; ?><div class="progress-bar progress-bar-danger" style="width: <?php echo $temperatura;?>%;"></div>
+				</div>
+				<h4>Memoria Disponibile</h4>
+				<div class="progress progress-striped">
+				  <?php echo $memoria_usata."/".$memoria_tot." GB"; ?><div class="progress-bar progress-bar-info" style="width: <?php echo $memoria_percentuale;?>%;"></div>
+				</div>
+				
+				<form method="POST" action='spegni_riavvia.php'>
+				<input type="submit" name="shutdown" value="Server Shutdown" class="btn btn-primary btn-large btn-block">
+				</form>
+				<div style="height:10px";"></div> <!-- div usato per distanziare verticalmente!! -->
+				<form method="POST" action='spegni_riavvia.php'>
+				<input type="submit" name="reboot" value="Server Reboot" class="btn btn-primary btn-large btn-block">
+				</form>
+				
 
             </div>
 
