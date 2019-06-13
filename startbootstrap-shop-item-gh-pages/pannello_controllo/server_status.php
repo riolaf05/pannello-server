66df60371fb9c0cf079e276b71afda7a0837bbe7
Rosario Laface <rosario.laface@nttdata.com>
2019-06-13 14:54:09 +0200

    Update server_status.php

--- startbootstrap-shop-item-gh-pages/pannello_controllo/server_status.php
+++ startbootstrap-shop-item-gh-pages/pannello_controllo/server_status.php
@@ -137,7 +137,7 @@
                 if (file_exists("/tmp/nodes_param.xml")) {
                     
                     $xmldata = simplexml_load_file("/tmp/nodes_param.xml") or die("Failed to load");    
-                    echo $xmldata;
+                    print_r($xmldata);
 
                      ?>
                 
