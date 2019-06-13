1aea82c89d1a4d7fddc4788eaf44687ccfe67d9c
Rosario Laface <rosario.laface@nttdata.com>
2019-06-13 15:52:39 +0200

    Update build.sh

--- scripts/circleci/build.sh
+++ scripts/circleci/build.sh
@@ -7,5 +7,5 @@ docker login -u $DOCKER_USER -p $DOCKER_PASS
 
 info "Pushing docker images"
 docker push rio05docker/inotify-video-converter:latest
-docker push rio05docker/web_server_panel:latest
+docker push rio05docker/web_server_panel:rpi3_latest
 
