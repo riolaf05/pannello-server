8d9f8091b8ff48c74d773fcb47ca5404e455e2c7
Rosario Laface <rosario.laface@nttdata.com>
2019-06-14 11:24:26 +0200

    Update build.sh

--- scripts/circleci/build.sh
+++ scripts/circleci/build.sh
@@ -1,11 +1,15 @@
+
+$DOCKER_USER = rio05docker
+$DOCKER_PASS = onslario89
+
 echo "Building docker images"
 docker build -t "rio05docker/inotify-video-converter:latest" inotify-video-converter/
-docker build -t "rio05docker/web_server_panel:latest" startbootstrap-shop-item-gh-pages/
+docker build -t "rio05docker/web_server_panel:rpi3_latest" startbootstrap-shop-item-gh-pages/
 
 echo "Login to docker hub"
 docker login -u $DOCKER_USER -p $DOCKER_PASS
 
 info "Pushing docker images"
 docker push rio05docker/inotify-video-converter:latest
-docker push rio05docker/web_server_panel:latest
+docker push rio05docker/web_server_panel:rpi3_latest
 
