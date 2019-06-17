defbaa86125fa77e1f52dd33af5a2ff3d1080a61
Rosario Laface <rosario.laface@nttdata.com>
2019-06-17 18:51:04 +0200

    Update build.sh

--- scripts/circleci/build.sh
+++ scripts/circleci/build.sh
@@ -4,12 +4,12 @@ DOCKER_TAG='rpi3_test_'$CIRCLE_BUILD_NUM #CHANGE THIS!
 
 echo "Building docker images"
 #docker build -t "rio05docker/inotify-video-converter:latest" inotify-video-converter/
-docker build -t "rio05docker/web_server_panel:rpi3_test_$DOCKER_TAG" startbootstrap-shop-item-gh-pages/
+docker build -t "rio05docker/web_server_panel:$DOCKER_TAG" startbootstrap-shop-item-gh-pages/
 
 echo "Login to docker hub"
 docker login -u $DOCKER_USER -p $DOCKER_PASS
 
 info "Pushing docker images"
 #docker push rio05docker/inotify-video-converter:latest
-docker push rio05docker/web_server_panel:rpi3_test_$DOCKER_TAG
+docker push rio05docker/web_server_panel:$DOCKER_TAG
 
