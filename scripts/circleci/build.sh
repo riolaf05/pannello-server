520fc2388ab97306fe5dd78d2475c090b9ad70af
Rosario Laface <rosario.laface@nttdata.com>
2019-06-17 18:48:52 +0200

    removing comments

--- scripts/circleci/build.sh
+++ scripts/circleci/build.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-#export RANDOM_TAG=$(cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 32 | head -n 1) #generate random hash tag for docker images
 DOCKER_TAG='rpi3_test_'$CIRCLE_BUILD_NUM #CHANGE THIS!
 
 echo "Building docker images"
