f7001aed1376e530af4509078c319f1dfee06597
Rosario Laface <rosario.laface@nttdata.com>
2019-06-18 16:44:13 +0200

    bug fix

--- scripts/circleci/push.sh
+++ scripts/circleci/push.sh
@@ -5,6 +5,6 @@ DOCKER_TAG='rpi3_'$CIRCLE_PREVIOUS_BUILD_NUM
 echo "Login to docker hub"
 docker login -u $DOCKER_USER -p $DOCKER_PASS
 
-info "Pushing docker images"
+echo "Pushing docker images"
 #docker push rio05docker/inotify-video-converter:$DOCKER_TAG
-docker push "rio05docker/web_server_panel:$DOCKER_TAG"
+docker push rio05docker/web_server_panel:$DOCKER_TAG
