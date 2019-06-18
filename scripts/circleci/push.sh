cb9fd2aad0b763cc6481ddc9a23d9134956304e6
Rosario Laface <rosario.laface@nttdata.com>
2019-06-18 12:49:04 +0200

    removing multiple jobs

--- scripts/circleci/push.sh
+++ scripts/circleci/push.sh
@@ -1,9 +1,9 @@
 
-DOCKER_TAG='rpi3_test_'$CIRCLE_SHA1 #CHANGE THIS!
+DOCKER_TAG='rpi3_'$CIRCLE_PREVIOUS_BUILD_NUM #CHANGE THIS!
 
 echo "Login to docker hub"
 docker login -u $DOCKER_USER -p $DOCKER_PASS
 
 info "Pushing docker images"
-#docker push rio05docker/inotify-video-converter:latest
+#docker push rio05docker/inotify-video-converter:$DOCKER_TAG
 docker push rio05docker/web_server_panel:$DOCKER_TAG
