dc54cccf7cbed08ec234b4a6a79fa5fe0b42c5ed
Rosario Laface <rosario.laface@nttdata.com>
2019-07-19 11:20:54 +0200

    Update build.sh

--- scripts/circleci/build.sh
+++ scripts/circleci/build.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-DOCKER_TAG='rpi3_'$CIRCLE_BUILD_NUM 
+DOCKER_TAG='rpi3_test_'$CIRCLE_BUILD_NUM 
 
 echo "Building docker images"
-docker build -t "rio05docker/inotify-video-converter:$DOCKER_TAG" inotify-video-converter/
+#docker build -t "rio05docker/inotify-video-converter:$DOCKER_TAG" inotify-video-converter/
 docker build -t "rio05docker/web_server_panel:$DOCKER_TAG" startbootstrap-shop-item-gh-pages/
 
 
