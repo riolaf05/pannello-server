305ccd9fd1d90fb28b687e118dfa325ad361237b
rosario89 <lafacerosario@gmail.com>
2019-07-21 20:13:38 +0200

    Update deploy.sh

--- startbootstrap-shop-item-gh-pages/deploy.sh
+++ startbootstrap-shop-item-gh-pages/deploy.sh
@@ -33,6 +33,6 @@ MONTH=$(date -d "$D" '+%m')
 DOCKER_TAG='rpi3_test_'$DAY'_'$MONTH
 
 echo "Build and push subscriber container"
-docker build -t "rio05docker/web_server_panel::"$DOCKER_TAG .
+docker build -t "rio05docker/web_server_panel:"$DOCKER_TAG .
 docker push rio05docker/web_server_panel::$DOCKER_TAG
 
