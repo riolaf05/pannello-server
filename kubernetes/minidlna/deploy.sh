a4838a02afd6a00380028a42d13aa9b5d9550b21
rosario89 <lafacerosario@gmail.com>
2019-07-21 20:49:51 +0200

    updating minidlna

--- kubernetes/minidlna/deploy.sh
+++ kubernetes/minidlna/deploy.sh
@@ -2,10 +2,10 @@
 
 #Build step, template files will be filled with env parameters
 NODE_HOSTNAME=raspberry.local
-sed 's|{{ .EXT_HD_PATH }}|/media/pi/extHD|g' resources/persistentVolume.yaml.tpl > resources/persistentVolume.yaml
+sed 's|{{ .EXT_HD_PATH }}|/media/pi/extHD1|g' resources/persistentVolume.yaml.tpl > resources/persistentVolume.yaml
 sed 's|{{ .NODE }}|$NODE_HOSTNAME|g' resources/persistentVolume.yaml.tpl > resources/persistentVolume.yaml
 
 #Deploy step, applying k8s resources
 kubectl apply -f resources/persistentVolume.yaml
 kubectl apply -f resources/persistentVolumeClaim.yaml
-kubectl apply -f resources/pod.yaml
+kubectl apply -f resources/deployment.yaml
