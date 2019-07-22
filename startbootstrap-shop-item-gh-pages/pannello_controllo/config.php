4ef2f3d674ee57ebf008d2dab9945bba2b52ac4a
Rosario Laface <rosario.laface@nttdata.com>
2019-07-22 12:03:24 +0200

    login page - adding login scripts

---
+++ startbootstrap-shop-item-gh-pages/pannello_controllo/config.php
@@ -0,0 +1,21 @@
+/generali
+
+	$sito_internet = "Rio's Cloud";
+
+	$data =(date("d-m-y"));
+
+	$vers = "2.0";
+
+	//URL PER HTACCESS
+
+	$base_url = "http://riohomeiot.dsss.net/";
+
+	//connessione DB
+
+	$host = "localhost";
+
+	$db_user = "root";
+
+	$db_psw = "wtargetweb";
+
+	$db_name = "target";
--- startbootstrap-shop-item-gh-pages/pannello_controllo/login.php
+++ startbootstrap-shop-item-gh-pages/pannello_controllo/login.php
@@ -1,43 +1,26 @@
+<?php include('config.php'); ?>
 <!DOCTYPE html>
-<html lang="en">
-
+<html>
 <head>
 
-    <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <meta name="description" content="">
-    <meta name="author" content="">
+    <title>Collegati per amministrare il sito - <?php echo $sito_internet ?></title>
 
-    <title>Pannello di Controllo</title>
-
-    <!-- Bootstrap Core CSS -->
-    <link href="css/bootstrap.min.css" rel="stylesheet">
-
-    <!-- Custom CSS -->
-    <link href="css/shop-item.css" rel="stylesheet">
-
-    <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
-    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
-    <!--[if lt IE 9]>
-        <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
-        <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
-    <![endif]-->
+    <!--Pannello di gestione creato da Mel Riccardo-->
+    <link href="css/login.css" rel="stylesheet" type="text/css" />
 
 </head>
-
 <body>
 
-	<div class="container">
-	<div class="row">
-    <div class="col-md-6 col-md-offset-3">
-		
-		
-		
-    </div>
-  </div>
-</div>
+    <form id="login" action="verifica.php" method="post">
+        <fieldset id="inputs">
+            <input id="username" name="username" type="text" placeholder="Username" autofocus required>
+            <input id="password" name="password" type="password" placeholder="Password" required>
+        </fieldset>
+        <fieldset id="actions">
+            <input type="submit" id="submit" value="Collegati">
+            <a href="../index.php" id="back">Ritorna al sito</a>
+        </fieldset>
+    </form>
 
 </body>
-
 </html>
