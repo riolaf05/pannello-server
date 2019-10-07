d3f911fc7513cea553b00584146ea48f8b51aea8
Rosario Laface <rosario.laface@nttdata.com>
2019-10-07 23:00:33 +0200

    Update internet_of_things.php

--- startbootstrap-shop-item-gh-pages/pannello_controllo/internet_of_things.php
+++ startbootstrap-shop-item-gh-pages/pannello_controllo/internet_of_things.php
@@ -177,7 +177,9 @@
                     <iframe width="450" height="260" style="border: 1px solid #cccccc;" src="https://thingspeak.com/channels/689988/widgets/41173"></iframe>
 
 
-                    <iframe width="450" height="260" style="border: 1px solid #cccccc;" src="https://thingspeak.com/channels/689988/charts/1?bgcolor=%23ffffff&color=%23d62020&dynamic=true&results=60&type=line&update=15"></iframe>
+                    <iframe width="450" height="260" style="border: 1px solid #cccccc;" src="https://thingspeak.com/channels/689988/widgets/88557"></iframe>
+
+
                 </div>
                 
 
