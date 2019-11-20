5fd32efa811274be8fed586a5a2d15e65bdae70a
riolaf05 <30504264+riolaf05@users.noreply.github.com>
2019-11-21 00:11:40 +0100

    Update api-server.py

--- scripts/restful-server/api-server.py
+++ scripts/restful-server/api-server.py
@@ -10,10 +10,10 @@ api = Api(app)
 class Camera(Resource):
     def get(self, command):
         if command == 'on':
-            rc = subprocess.call("/home/pi/Scripts/test.sh")
+            rc = subprocess.call("/home/pi/Scripts/motion-start.sh")
             return redirect("http://riohomecloud.ddns.net/pannello_controllo/server_status.php", code=302)
         else:
-            rc = subprocess.call("/home/pi/Scripts/test.sh")
+            rc = subprocess.call("/home/pi/Scripts/motion-stop.sh")
             return redirect("http://riohomecloud.ddns.net/pannello_controllo/server_status.php", code=302)
 
 class Docker(Resource):
@@ -25,4 +25,4 @@ api.add_resource(Camera, '/camera/<command>') # Route_1
 api.add_resource(Docker, '/minidlna') # Route_2
 
 if __name__ == '__main__':
-     app.run(port='5002')
+     app.run(host='0.0.0.0', port='5002')
