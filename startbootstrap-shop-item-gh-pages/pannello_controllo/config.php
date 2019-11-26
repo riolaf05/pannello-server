14bed40564dddf5c20bffd53e83dfa33f926263a
Rosario Laface <rosario.laface@nttdata.com>
2019-11-26 16:38:28 +0100

    changing mysql endpoint

--- startbootstrap-shop-item-gh-pages/pannello_controllo/config.php
+++ startbootstrap-shop-item-gh-pages/pannello_controllo/config.php
@@ -1,5 +1,5 @@
 <?php
-   define('DB_SERVER', '192.168.1.9'); #TODO: expose MySQL as k8s service and remove hard-coded IP 
+   define('DB_SERVER', '192.168.1.1');  
    define('DB_USERNAME', 'root');
    define('DB_PASSWORD', 'onslario89');
    define('DB_DATABASE', 'Login');
