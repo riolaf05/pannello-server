2eff19fad5c84df1186cffc5a568c565136238df
Rosario Laface <rosario.laface@nttdata.com>
2020-10-11 17:51:04 +0200

    Update install.sh

--- kubernetes/traefik/install.sh
+++ kubernetes/traefik/install.sh
@@ -1,3 +1,4 @@
+#see: https://unixorn.github.io/post/k3s-on-arm/
 for traefik_yaml in traefik-rbac.yaml traefik-configmap.yaml traefik-deployment.yaml traefik-service.yaml
 do
   kubectl apply -f $traefik_yaml
